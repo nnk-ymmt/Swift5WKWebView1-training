715db28603fbfcd7879f454431c12e72ce6f543f
Nonoka Yamamoto <garden.n1234567890@gmail.com>
2020-04-23 12:18:22 +0900

    Initial Commit

---
+++ Swift5WKWebView1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swift5WKWebView1
+//
+//  Created by 山本ののか on 2020/04/23.
+//  Copyright © 2020 Nonoka Yamamoto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
